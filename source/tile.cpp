0660324576f79b948673ae43de351fffd0d25d91
Olle Olsson <ohjolsson@gmail.com>
2013-05-03 21:52:06 +0200

    Added missing file

---
+++ source/tile.cpp
@@ -0,0 +1,35 @@
+#include "tile.h"
+//=============================================================================
+void Tile::setColor(Image c)
+{
+	color = c;
+}
+//=============================================================================
+Image Tile::getColor() const
+{
+	return color;
+}
+//=============================================================================
+void Tile::clearListeners()
+{
+	listeners.clear();
+}
+//=============================================================================
+
+void Tile::broadcastEnter(int mobid) const
+{
+	for(unsigned int i=0; i < listeners.size(); i++)
+		listeners[i]->mobEntered(mobid);
+}
+//=============================================================================
+void Tile::broadcastExit(int mobId) const
+{
+	for(unsigned int i=0; i < listeners.size(); i++)
+		listeners[i]->mobLeft(mobId);
+}
+//=============================================================================
+void Tile::addListener(TowerListener *t)
+{
+	listeners.push_back(t);
+}
+//=============================================================================
